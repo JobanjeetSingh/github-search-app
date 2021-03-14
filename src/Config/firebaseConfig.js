86a1d3625479d7f43b249f4091b20e493a558d38
Jobanjeet  Singh <jobanjeetsingh@Jobanjeets-Mac-mini.local>
2021-03-14 16:55:02 -0700

    last commit

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyBGrWZ4BK4wXxQGmROpQBuMSVe4XF9XvJE",
+  authDomain: "joban-git-app.firebaseapp.com",
+  databaseURL: "https://joban-git-app.firebaseio.com",
+  projectId: "joban-git-app",
+  storageBucket: "joban-git-app.appspot.com",
+  messagingSenderId: "294424312318",
+  appId: "1:294424312318:web:7ff7ebbdf63d721606e8af",
+  measurementId: "G-649474N610",
+};
